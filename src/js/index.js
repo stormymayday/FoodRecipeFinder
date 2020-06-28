accc55065e30d5defe866f785b1d1869c7b6cbdc
stormymayday <stormymayday7@gmail.com>
2020-06-28 17:56:44 -0600

    Implemented the Search model.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/** Global State
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+
+    // Getting query from the view
+    const query = 'pizza';
+
+    if (query) {
+
+        // Creating new Search object and storing it in the Global State object
+        state.search = new Search(query);
+
+        // Prepearing the UI
+
+        // Searching the recipes
+        await state.search.getResults();
+
+        // Rendering the results
+        console.log(state.search.result);
+
+    }
+
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    controlSearch();
+
+});
+
