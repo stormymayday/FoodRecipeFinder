aa5f2ff79b0415a395be757fd767a7d10e1e3e6d
stormymayday <stormymayday7@gmail.com>
2020-07-02 10:27:42 -0600

    Implemented the shopping list View.

---
+++ src/js/views/listView.js
@@ -0,0 +1,30 @@
+import { elements } from './base';
+
+export const renderItem = (item) => {
+	// Creating markup
+	const markup = `
+        <li class="shopping__item" data-itemid=${item.id}>
+            <div class="shopping__count">
+                <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
+                <p>${item.unit}</p>
+            </div>
+            <p class="shopping__description">${item.ingredient}</p>
+            <button class="shopping__delete btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-cross"></use>
+                </svg>
+            </button>
+        </li>
+    `;
+
+	// Inserting markup
+	elements.shopping.insertAdjacentHTML('beforeend', markup);
+};
+
+export const deleteItem = (id) => {
+	// Selecting an item based on id
+	const item = document.querySelector(`[data-itemid="${id}"]`);
+
+	// Removing item from the UI
+	item.parentNode.removeChild(item);
+};
