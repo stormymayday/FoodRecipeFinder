5cb2c9dc1652482cae56d18d971a79e68cbdf311
stormymayday <stormymayday7@gmail.com>
2023-09-17 18:02:52 -0700

    Removed javascript.svg

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+// import javascriptLogo from './javascript.svg'
 import logo from '/favicon.png'
 import { setupCounter } from './counter.js'
 
@@ -7,9 +7,6 @@ document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${logo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
